6cb69c88a4b0cb01e15cc8b42eabeaab276cca89
sungchan <awdr1057@naver.com>
2026-05-25 17:56:37 +0900

    feat: 도메인, 레포지토리 (Video, RabbitMQ) 준비

---
+++ src/main/java/com/example/capstoneproject220261/repository/VideoRepository.java
@@ -0,0 +1,12 @@
+package com.example.capstoneproject220261.repository;
+
+import com.example.capstoneproject220261.domain.Video;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VideoRepository extends JpaRepository<Video, Long> {
+
+  Optional<Video> findByJobId(String jobId);
+}
