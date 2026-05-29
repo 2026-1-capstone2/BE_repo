3f31ff2badd6128198ad407bde70ad1b358c06ba
sungchan <awdr1057@naver.com>
2026-05-29 17:34:56 +0900

    doxs: Redis 기본 준비

--- src/main/java/com/example/capstoneproject220261/CapstoneProject220261Application.java
+++ src/main/java/com/example/capstoneproject220261/CapstoneProject220261Application.java
@@ -2,8 +2,10 @@ package com.example.capstoneproject220261;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class CapstoneProject220261Application {
 
   public static void main(String[] args) {
